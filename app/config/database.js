8d6bee35f8917ef4ed5c079f807e63da31bd52d6
programmermedical <programmermedical@gmail.com>
2024-08-27 02:01:53 +0700

    update config vercel

--- app/config/database.js
+++ app/config/database.js
@@ -1,6 +1,7 @@
 // config/database.js
 const { Sequelize } = require('sequelize');
 const config = require('../../config/config.js');
+const pg = require('pg');
 
 const environment = process.env.NODE_ENV || 'production';
 const envConfig = config[environment];
@@ -13,6 +14,7 @@ const sequelize = new Sequelize(
     host: envConfig.host,
     dialect: envConfig.dialect,
     logging: envConfig.logging || false,
+    dialectModule: pg
   },
 );
 
